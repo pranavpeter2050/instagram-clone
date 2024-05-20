7a1d4168ec4b6048d1eba721e380045f8ef910d1
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-21 02:38:28 +0530

    added sideNav and TopNav which shows/hides based on screen size

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -12,15 +12,85 @@ import AccountOutline from 'vue-material-design-icons/AccountOutline.vue'
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
 import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue'
 
+import MenuItem from '@/Components/MenuItem.vue';
+
 let showCreatePost = ref(false)
 </script>
 
 <template>
-  <div id="MainLayout" class="bg-red-600">
-    <div>
-      MainLayout
+  <div id="MainLayout" class="w-full h-screen">
+    <div v-if="$page.url === '/'" id="TopNavHome" class="fixed z-30 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300">
+      <div class="flex items-center justify-between h-full">
+        <Link href="/">
+          <img src="/insta-logo.png" class="w-[105px] ml-6 cursor-pointer">
+        </Link>
+
+        <div class="flex items-center w-[50%]">
+          <div class="flex items-center w-full bg-gray-100 rounded-lg">
+            <Magnify class="pl-4" fillColor="#8e8e8e" :size="27" />
+            <input
+              type="text"
+              placeholder="Search"
+              class="
+                bg-transparent
+                w-full
+                placeholder-[#8e8e8e]
+                border-0
+                ring-0
+                focus:ring-0
+              "
+            >
+          </div>
+
+          <HeartOutline class="pl-4 pr-3" fillColor="#000000" :size="27" />
+        </div>
+      </div>
     </div>
 
-    <slot />
+    <div
+      v-if="$page.url !== '/'"
+      id="TopNavUser"
+      class="mg:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300"
+    >
+      <Link class="px-4">
+        <ChevronLeft :size="30" class="cursor-pointer" />
+      </Link>
+      <div class="font-extrabold text-lg">Name Here</div>
+      <AccountPlusOutline :size="30" class="cursor-pointer px-4" />
+    </div>
+
+    <div id="SideNav" class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300">
+      <Link href="/">
+        <img src="/insta-logo-small.png" class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer">
+        <img src="/insta-logo.png" class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer">
+      </Link>
+
+      <div class="px-3">
+        <Link href="/">
+          <MenuItem iconString="Home" class="mb-4" />
+        </Link>
+        <MenuItem iconString="Search" class="mb-4" />
+        <MenuItem iconString="Explore" class="mb-4" />
+        <MenuItem iconString="Messages" class="mb-4" />
+        <MenuItem iconString="Notifications" class="mb-4" />
+        <MenuItem iconString="Profile" class="mb-4" />
+      </div>
+
+      <Link href="/" class="absolute bottom-0 px-3 w-full">
+        <MenuItem iconString="Log out" class="mb-4" />
+      </Link>
+    </div>
+
+    <div class="flex lg:justify-between bg-white h-full w-[100%-280px] xl:pl-[280px] lg:pl-[100px] overflow-auto">
+      <div
+        class="mx-auto md:pt-6 pt-20"
+        :class="$page.url === '/' ? 'lg:w-8/12 w-full' : 'max-w-[1200px]'"
+      >
+        <main>
+          <slot />
+        </main>
+      </div>
+    </div>
+
   </div>
 </template>
