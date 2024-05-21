5f92aa3e443de1eda54ced61abd98e53a3fa41a1
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-21 21:46:45 +0530

    updated auth scaffolding related Login, Register vue files

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -24,9 +24,12 @@ const submit = () => {
     <GuestLayout>
         <Head title="Register" />
 
+        <img src="/insta-logo.png" width="200" class="mx-auto pt-8 pb-6">
+
+        <div class="text-gray-600 text-center font-extrabold text-[22px] mb-4">Sign up to see photos and videos from your friends.</div>
+
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="name" value="Name" />
 
                 <TextInput
                     id="name"
@@ -36,13 +39,13 @@ const submit = () => {
                     required
                     autofocus
                     autocomplete="name"
+                    placeholder="Name"
                 />
 
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
 
             <div class="mt-4">
-                <InputLabel for="email" value="Email" />
 
                 <TextInput
                     id="email"
@@ -51,13 +54,13 @@ const submit = () => {
                     v-model="form.email"
                     required
                     autocomplete="username"
+                    placeholder="Email"
                 />
 
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
 
                 <TextInput
                     id="password"
@@ -66,13 +69,13 @@ const submit = () => {
                     v-model="form.password"
                     required
                     autocomplete="new-password"
+                    placeholder="Password"
                 />
 
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
 
                 <TextInput
                     id="password_confirmation"
@@ -81,22 +84,33 @@ const submit = () => {
                     v-model="form.password_confirmation"
                     required
                     autocomplete="new-password"
+                    placeholder="Confirm Password"
                 />
 
                 <InputError class="mt-2" :message="form.errors.password_confirmation" />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <PrimaryButton class="mt-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                Register
+            </PrimaryButton>
+
+            <div class="flex items-center justify-center mt-4">
                 <Link
                     :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Already registered?
                 </Link>
+            </div>
 
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </PrimaryButton>
+            <div class="flex items-center justify-center mt-4">
+                Have an account?
+                <Link
+                    :href="route('login')"
+                    class="pl-1 font-bold text-blue-600 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Log in
+                </Link>
             </div>
         </form>
     </GuestLayout>
