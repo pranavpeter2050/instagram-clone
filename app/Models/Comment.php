7f83b15a1e1d460450e014c21899bc2220263f13
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-22 15:21:31 +0530

    defined the (inter) table relations in the various "Model" files

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post() {
+        // return $this->belongsTo(Post::class, 'id', 'post_id'); // by default, so we don't need to specify the columns, refer laravel documentation on "belongsTo" or "oneToMany"
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
